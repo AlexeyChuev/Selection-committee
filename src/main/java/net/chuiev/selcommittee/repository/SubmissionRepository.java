5fcb9cb8a2ea5ff6f3f048443f73396e7974af1b
Oleksii Chuiev <alexeychuev@mail.ru>
2016-03-05 17:06:54 +0000

    add Submission + SubmissionRepository

---
+++ src/main/java/net/chuiev/selcommittee/repository/SubmissionRepository.java
@@ -0,0 +1,91 @@
+package net.chuiev.selcommittee.repository;
+
+import net.chuiev.selcommittee.repository.entity.Submission;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * Created by Алексей on 3/5/2016.
+ */
+public class SubmissionRepository implements Repository<Submission> {
+    private Connection connection = ConnectionPoolFactory.getConnection();
+
+    @Override
+    public void create(Submission entity) throws SQLException {
+        String sql = "INSERT INTO Submission VALUES(?,?,?);";
+        try(PreparedStatement preparedStatement = connection.prepareStatement(sql))
+        {
+            preparedStatement.setInt(1, entity.getId());
+            preparedStatement.setInt(2, entity.getFacultyId());
+            preparedStatement.setInt(3, entity.getEnrolleeId());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void update(Submission oldEntity, Submission newEntity) throws SQLException {
+        String sql = "UPDATE Submission SET facultyId=?, enrolleeId=? WHERE id=?;";
+        try(PreparedStatement preparedStatement = connection.prepareStatement(sql))
+        {
+            preparedStatement.setInt(1, newEntity.getFacultyId());
+            preparedStatement.setInt(2, newEntity.getEnrolleeId());
+            preparedStatement.setInt(3, oldEntity.getId());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void delete(Submission entity) throws SQLException {
+        String sql = "DELETE FROM Submission WHERE id=" + entity.getId() + ";";
+        try {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(sql);
+        }
+        catch (SQLException e){e.printStackTrace();}
+    }
+
+    @Override
+    public Submission get(Submission entity) throws SQLException {
+        String sql = "SELECT * FROM Submission WHERE id="+entity.getId()+ ";";
+        Submission newSubmission = new Submission();
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+            resultSet.next();
+            newSubmission.setId(resultSet.getInt("id"));
+            newSubmission.setFacultyId(resultSet.getInt("facultyId"));
+            newSubmission.setEnrolleeId(resultSet.getInt("enrolleeId"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return newSubmission;
+    }
+
+    @Override
+    public Collection<Submission> findAll() throws SQLException {
+        String sql = "SELECT * FROM Submission;";
+        Collection<Submission> submissions = new ArrayList<>();
+        Statement statement;
+        try {
+            statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+            while (resultSet.next())
+            {
+                Submission newSubmission = new Submission();
+                newSubmission.setId(resultSet.getInt("id"));
+                newSubmission.setFacultyId(resultSet.getInt("facultyId"));
+                newSubmission.setEnrolleeId(resultSet.getInt("enrolleeId"));
+                submissions.add(newSubmission);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return submissions;
+    }
+}
---
+++ src/main/java/net/chuiev/selcommittee/repository/entity/Submission.java
@@ -0,0 +1,51 @@
+package net.chuiev.selcommittee.repository.entity;
+
+/**
+ * Created by Алексей on 3/5/2016.
+ */
+public class Submission {
+    private int id;
+    private int facultyId;
+    private int enrolleeId;
+
+    public Submission(int id, int facultyId, int enrolleeId) {
+        this.id = id;
+        this.facultyId = facultyId;
+        this.enrolleeId = enrolleeId;
+    }
+
+    public Submission(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getFacultyId() {
+        return facultyId;
+    }
+
+    public void setFacultyId(int facultyId) {
+        this.facultyId = facultyId;
+    }
+
+    public int getEnrolleeId() {
+        return enrolleeId;
+    }
+
+    public void setEnrolleeId(int enrolleeId) {
+        this.enrolleeId = enrolleeId;
+    }
+
+    @Override
+    public String toString() {
+        return "Submission{" +
+                "id=" + id +
+                ", facultyId=" + facultyId +
+                ", enrolleeId=" + enrolleeId +
+                '}';
+    }
+}
