c3287d4fabc197ee39ca2e5695df4c28307626bd
Oleksii Chuiev <alexeychuev@mail.ru>
2016-03-12 11:35:35 +0000

    add CONSTRANTS ON DELETE CASCADE, ON UPDATE RESTRICT to initial sql remove id from constructor Subject

--- src/main/java/net/chuiev/selcommittee/entity/Subject.java
+++ src/main/java/net/chuiev/selcommittee/entity/Subject.java
@@ -11,8 +11,8 @@ public class Subject implements Serializable {
     private int id;
     private String name;
 
-    public Subject(int id, String name) {
-        this.id = id;
+    public Subject(String name) {
+
         this.name = name;
     }
 
