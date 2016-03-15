06ab3f294e0af11ee64a8ab5b27cf5c2710b63bc
Oleksii Chuiev <alexeychuev@mail.ru>
2016-03-15 17:05:46 +0000

    add english names for faculties, subjects add 40 enrollees add half of all submissions (45)

---
+++ src/main/sql/enrolleeData.sql
@@ -0,0 +1,120 @@
+
+INSERT into ADMIN.USERS (ROLE, EMAIL, PASSWORD, ISBLOCKED) VALUES (2, 'davidbeckham@gmail.com', 'davidbeckham', FALSE);
+INSERT INTO ADMIN.ENROLLEE (FULL_NAME, CITY, REGION, SCHOOL_NAME, CERTIFICATE, USER_ID) VALUES ('David Beckham', 'London', 'England', 'Private School 3', NULL, 2);
+
+INSERT into ADMIN.USERS (ROLE, EMAIL, PASSWORD, ISBLOCKED) VALUES (2, 'zinedinzidan@gmail.com', 'zinedinzidan', FALSE);
+INSERT INTO ADMIN.ENROLLEE (FULL_NAME, CITY, REGION, SCHOOL_NAME, CERTIFICATE, USER_ID) VALUES ('Zinedin Zidan', 'Paris', 'Shampaghn', 'School 145', NULL, 3);
+
+INSERT into ADMIN.USERS (ROLE, EMAIL, PASSWORD, ISBLOCKED) VALUES (2, 'andriishevchenko@gmail.com', 'andriishevchenko', FALSE);
+INSERT INTO ADMIN.ENROLLEE (FULL_NAME, CITY, REGION, SCHOOL_NAME, CERTIFICATE, USER_ID) VALUES ('Andrii Shevchenko', 'Kiev', 'Ukraine', 'City school 32', NULL, 4);
+
+INSERT into ADMIN.USERS (ROLE, EMAIL, PASSWORD, ISBLOCKED) VALUES (2, 'andriishovkovskii@gmail.com', 'andriishovkovskii', FALSE);
+INSERT INTO ADMIN.ENROLLEE (FULL_NAME, CITY, REGION, SCHOOL_NAME, CERTIFICATE, USER_ID) VALUES ('Andrii Shovkovskii', 'Kiev', 'Ukraine', 'School 12', NULL, 5);
+
+INSERT into ADMIN.USERS (ROLE, EMAIL, PASSWORD, ISBLOCKED) VALUES (2, 'lionelmessi@gmail.com', 'lionelmessi', FALSE);
+INSERT INTO ADMIN.ENROLLEE (FULL_NAME, CITY, REGION, SCHOOL_NAME, CERTIFICATE, USER_ID) VALUES ('Lionel Messi', 'Buenos Aires', 'Argentina', 'School 39', NULL, 6);
+
+INSERT into ADMIN.USERS (ROLE, EMAIL, PASSWORD, ISBLOCKED) VALUES (2, 'cristianoronaldo@gmail.com', 'cristianoronaldo', FALSE);
+INSERT INTO ADMIN.ENROLLEE (FULL_NAME, CITY, REGION, SCHOOL_NAME, CERTIFICATE, USER_ID) VALUES ('Cristiano Ronaldo', 'Lisabon', 'Porugal', 'Private School 123', NULL, 7);
+
+INSERT into ADMIN.USERS (ROLE, EMAIL, PASSWORD, ISBLOCKED) VALUES (2, 'arienrobben@gmail.com', 'arienrobben', FALSE);
+INSERT INTO ADMIN.ENROLLEE (FULL_NAME, CITY, REGION, SCHOOL_NAME, CERTIFICATE, USER_ID) VALUES ('Arien Robben', 'Amsterdam', 'Holland', 'Peter Parker School', NULL, 8);
+
+INSERT into ADMIN.USERS (ROLE, EMAIL, PASSWORD, ISBLOCKED) VALUES (2, 'janlyidgibuffon@gmail.com', 'janlyidgibuffon', FALSE);
+INSERT INTO ADMIN.ENROLLEE (FULL_NAME, CITY, REGION, SCHOOL_NAME, CERTIFICATE, USER_ID) VALUES ('Jan Lyidgi Buffon', 'Rome', 'Italy', 'School 77', NULL, 9);
+
+INSERT into ADMIN.USERS (ROLE, EMAIL, PASSWORD, ISBLOCKED) VALUES (2, 'johnbrown@gmail.com', 'johnbrown', FALSE);
+INSERT INTO ADMIN.ENROLLEE (FULL_NAME, CITY, REGION, SCHOOL_NAME, CERTIFICATE, USER_ID) VALUES ('Josh Brown', 'Manchester', 'England', 'Private School 12', NULL, 10);
+
+INSERT into ADMIN.USERS (ROLE, EMAIL, PASSWORD, ISBLOCKED) VALUES (2, 'harrypotter@gmail.com', 'harrypotter', FALSE);
+INSERT INTO ADMIN.ENROLLEE (FULL_NAME, CITY, REGION, SCHOOL_NAME, CERTIFICATE, USER_ID) VALUES ('Harry Potter', 'London', 'England', 'Hogvarts', NULL, 11);
+
+INSERT into ADMIN.USERS (ROLE, EMAIL, PASSWORD, ISBLOCKED) VALUES (2, 'hermionegreinjer@gmail.com', 'germiongreinjer', FALSE);
+INSERT INTO ADMIN.ENROLLEE (FULL_NAME, CITY, REGION, SCHOOL_NAME, CERTIFICATE, USER_ID) VALUES ('Hermione Greinjer', 'Swoence', 'Scotland', 'Hogvarts', NULL, 12);
+
+INSERT into ADMIN.USERS (ROLE, EMAIL, PASSWORD, ISBLOCKED) VALUES (2, 'ronyezlee@gmail.com', 'ronyezlee', FALSE);
+INSERT INTO ADMIN.ENROLLEE (FULL_NAME, CITY, REGION, SCHOOL_NAME, CERTIFICATE, USER_ID) VALUES ('Ron Yezlee', 'Liverpool', 'England', 'Hogvarts', NULL, 13);
+
+INSERT into ADMIN.USERS (ROLE, EMAIL, PASSWORD, ISBLOCKED) VALUES (2, 'albusdambldor@gmail.com', 'albusdambldor', FALSE);
+INSERT INTO ADMIN.ENROLLEE (FULL_NAME, CITY, REGION, SCHOOL_NAME, CERTIFICATE, USER_ID) VALUES ('Albus Dambldor', 'Manchester', 'England', 'Hogvarts', NULL, 14);
+
+INSERT into ADMIN.USERS (ROLE, EMAIL, PASSWORD, ISBLOCKED) VALUES (2, 'ericcrocs@gmail.com', 'ericcrocs', FALSE);
+INSERT INTO ADMIN.ENROLLEE (FULL_NAME, CITY, REGION, SCHOOL_NAME, CERTIFICATE, USER_ID) VALUES ('Eric Crocs', 'New York', 'State Vashington', 'Abraham Linkoln School', NULL, 15);
+
+INSERT into ADMIN.USERS (ROLE, EMAIL, PASSWORD, ISBLOCKED) VALUES (2, 'larryjames@gmail.com', 'larryjames', FALSE);
+INSERT INTO ADMIN.ENROLLEE (FULL_NAME, CITY, REGION, SCHOOL_NAME, CERTIFICATE, USER_ID) VALUES ('Larry James', 'Las-Vegas', 'State Nevada', 'School 178', NULL, 16);
+
+INSERT into ADMIN.USERS (ROLE, EMAIL, PASSWORD, ISBLOCKED) VALUES (2, 'jamesnicklson@gmail.com', 'jamesnicklson', FALSE);
+INSERT INTO ADMIN.ENROLLEE (FULL_NAME, CITY, REGION, SCHOOL_NAME, CERTIFICATE, USER_ID) VALUES ('James Nicklson', 'Dallas', 'Texas', 'City School 44', NULL, 17);
+
+INSERT into ADMIN.USERS (ROLE, EMAIL, PASSWORD, ISBLOCKED) VALUES (2, 'anjelinajolly@gmail.com', 'anjelinajolly', FALSE);
+INSERT INTO ADMIN.ENROLLEE (FULL_NAME, CITY, REGION, SCHOOL_NAME, CERTIFICATE, USER_ID) VALUES ('Anjelina Jolly', 'Chicago', 'Illinois', 'Saint Edward School', NULL, 18);
+
+INSERT into ADMIN.USERS (ROLE, EMAIL, PASSWORD, ISBLOCKED) VALUES (2, 'leonardodicaprio@gmail.com', 'leonardodicaprio', FALSE);
+INSERT INTO ADMIN.ENROLLEE (FULL_NAME, CITY, REGION, SCHOOL_NAME, CERTIFICATE, USER_ID) VALUES ('Leonardo Dicaprio', 'Hollywood', 'California', 'Art School 111', NULL, 19);
+
+INSERT into ADMIN.USERS (ROLE, EMAIL, PASSWORD, ISBLOCKED) VALUES (2, 'elissabettailor@gmail.com', 'elissabettailor', FALSE);
+INSERT INTO ADMIN.ENROLLEE (FULL_NAME, CITY, REGION, SCHOOL_NAME, CERTIFICATE, USER_ID) VALUES ('Elissabet Tailor', 'Santa Fe', 'New Mexico', 'School 190', NULL, 20);
+
+INSERT into ADMIN.USERS (ROLE, EMAIL, PASSWORD, ISBLOCKED) VALUES (2, 'miketaison@gmail.com', 'miketaison', FALSE);
+INSERT INTO ADMIN.ENROLLEE (FULL_NAME, CITY, REGION, SCHOOL_NAME, CERTIFICATE, USER_ID) VALUES ('Mike Taison', 'Phoenix', 'Arizona', 'Box School', NULL, 21);
+
+INSERT into ADMIN.USERS (ROLE, EMAIL, PASSWORD, ISBLOCKED) VALUES (2, 'denzelvashington@gmail.com', 'denzelvashington', FALSE);
+INSERT INTO ADMIN.ENROLLEE (FULL_NAME, CITY, REGION, SCHOOL_NAME, CERTIFICATE, USER_ID) VALUES ('Denzel Vashington', 'Topeka', 'Cansas', 'School 12', NULL, 22);
+
+INSERT into ADMIN.USERS (ROLE, EMAIL, PASSWORD, ISBLOCKED) VALUES (2, 'ladygaga@gmail.com', 'ladygaga', FALSE);
+INSERT INTO ADMIN.ENROLLEE (FULL_NAME, CITY, REGION, SCHOOL_NAME, CERTIFICATE, USER_ID) VALUES ('Lady Gaga', 'Indianapolis', 'Indiana', 'Art School 40', NULL, 23);
+
+INSERT into ADMIN.USERS (ROLE, EMAIL, PASSWORD, ISBLOCKED) VALUES (2, 'cristinaagilerra@gmail.com', 'cristinaagilerra', FALSE);
+INSERT INTO ADMIN.ENROLLEE (FULL_NAME, CITY, REGION, SCHOOL_NAME, CERTIFICATE, USER_ID) VALUES ('Cristina Agilerra', 'Montgomerry', 'Alabama', 'Louis Vegan School', NULL, 24);
+
+INSERT into ADMIN.USERS (ROLE, EMAIL, PASSWORD, ISBLOCKED) VALUES (2, 'jessicaparker@gmail.com', 'jessicaparker', FALSE);
+INSERT INTO ADMIN.ENROLLEE (FULL_NAME, CITY, REGION, SCHOOL_NAME, CERTIFICATE, USER_ID) VALUES ('Jessica Parker', 'Orlando', 'Florida', 'School 1', NULL, 25);
+
+INSERT into ADMIN.USERS (ROLE, EMAIL, PASSWORD, ISBLOCKED) VALUES (2, 'merilynmonroe@gmail.com', 'merilynmonroe', FALSE);
+INSERT INTO ADMIN.ENROLLEE (FULL_NAME, CITY, REGION, SCHOOL_NAME, CERTIFICATE, USER_ID) VALUES ('Merilyn Monroe', 'Oklahoma City', 'Oklahoma', 'Curt Cobain School', NULL, 26);
+
+INSERT into ADMIN.USERS (ROLE, EMAIL, PASSWORD, ISBLOCKED) VALUES (2, 'tonystark@gmail.com', 'tonystark', FALSE);
+INSERT INTO ADMIN.ENROLLEE (FULL_NAME, CITY, REGION, SCHOOL_NAME, CERTIFICATE, USER_ID) VALUES ('Tony Stark', 'New York', 'New Jersey', 'Technical Newtons School', NULL, 27);
+
+INSERT into ADMIN.USERS (ROLE, EMAIL, PASSWORD, ISBLOCKED) VALUES (2, 'captainamerica@gmail.com', 'captainamerica', FALSE);
+INSERT INTO ADMIN.ENROLLEE (FULL_NAME, CITY, REGION, SCHOOL_NAME, CERTIFICATE, USER_ID) VALUES ('Captain America', 'Atalanta', 'Georgia', 'Military School 1', NULL, 28);
+
+INSERT into ADMIN.USERS (ROLE, EMAIL, PASSWORD, ISBLOCKED) VALUES (2, 'ashleecole@gmail.com', 'ashleecole', FALSE);
+INSERT INTO ADMIN.ENROLLEE (FULL_NAME, CITY, REGION, SCHOOL_NAME, CERTIFICATE, USER_ID) VALUES ('Ashlee Cole', 'Salt Lake City', 'Utah', 'James Brown School', NULL, 29);
+
+INSERT into ADMIN.USERS (ROLE, EMAIL, PASSWORD, ISBLOCKED) VALUES (2, 'louisgriffin@gmail.com', 'louisgriffin', FALSE);
+INSERT INTO ADMIN.ENROLLEE (FULL_NAME, CITY, REGION, SCHOOL_NAME, CERTIFICATE, USER_ID) VALUES ('Louis Griffin', 'Quahog', 'Oklahoma', 'School 82', NULL, 30);
+
+INSERT into ADMIN.USERS (ROLE, EMAIL, PASSWORD, ISBLOCKED) VALUES (2, 'petergriffin@gmail.com', 'petergriffin', FALSE);
+INSERT INTO ADMIN.ENROLLEE (FULL_NAME, CITY, REGION, SCHOOL_NAME, CERTIFICATE, USER_ID) VALUES ('Peter Griffin', 'Quahog', 'Oklahoma', 'School 3', NULL, 31);
+
+INSERT into ADMIN.USERS (ROLE, EMAIL, PASSWORD, ISBLOCKED) VALUES (2, 'kennymackormik@gmail.com', 'kennymackormik', FALSE);
+INSERT INTO ADMIN.ENROLLEE (FULL_NAME, CITY, REGION, SCHOOL_NAME, CERTIFICATE, USER_ID) VALUES ('Kenny Mackormik', 'South Park', 'Kansas', 'School 2', NULL, 32);
+
+INSERT into ADMIN.USERS (ROLE, EMAIL, PASSWORD, ISBLOCKED) VALUES (2, 'stanlyblack@gmail.com', 'stanlyblack', FALSE);
+INSERT INTO ADMIN.ENROLLEE (FULL_NAME, CITY, REGION, SCHOOL_NAME, CERTIFICATE, USER_ID) VALUES ('Stanly Black', 'Rock Springs', 'Wyoming', 'School 11', NULL, 33);
+
+INSERT into ADMIN.USERS (ROLE, EMAIL, PASSWORD, ISBLOCKED) VALUES (2, 'billygrey@gmail.com', 'billygrey', FALSE);
+INSERT INTO ADMIN.ENROLLEE (FULL_NAME, CITY, REGION, SCHOOL_NAME, CERTIFICATE, USER_ID) VALUES ('Billy Grey', 'Montgomerry', 'Alabama', 'Private School 1', NULL, 34);
+
+INSERT into ADMIN.USERS (ROLE, EMAIL, PASSWORD, ISBLOCKED) VALUES (2, 'hollyberry@gmail.com', 'hollyberry', FALSE);
+INSERT INTO ADMIN.ENROLLEE (FULL_NAME, CITY, REGION, SCHOOL_NAME, CERTIFICATE, USER_ID) VALUES ('Holly Berry', 'Oklahoma City', 'Oklahoma', 'School 267', NULL, 35);
+
+INSERT into ADMIN.USERS (ROLE, EMAIL, PASSWORD, ISBLOCKED) VALUES (2, 'martymackfky@gmail.com', 'martymackfky', FALSE);
+INSERT INTO ADMIN.ENROLLEE (FULL_NAME, CITY, REGION, SCHOOL_NAME, CERTIFICATE, USER_ID) VALUES ('Marty Mackfly', 'Bismarck', 'North Dakota', 'Tony Stark School', NULL, 36);
+
+INSERT into ADMIN.USERS (ROLE, EMAIL, PASSWORD, ISBLOCKED) VALUES (2, 'johnwaine@gmail.com', 'johnwaine', FALSE);
+INSERT INTO ADMIN.ENROLLEE (FULL_NAME, CITY, REGION, SCHOOL_NAME, CERTIFICATE, USER_ID) VALUES ('John Waine', 'Carson City', 'Nevada', 'School 14', NULL, 37);
+
+INSERT into ADMIN.USERS (ROLE, EMAIL, PASSWORD, ISBLOCKED) VALUES (2, 'jackychan@gmail.com', 'jackychan', FALSE);
+INSERT INTO ADMIN.ENROLLEE (FULL_NAME, CITY, REGION, SCHOOL_NAME, CERTIFICATE, USER_ID) VALUES ('Jecky Chan', 'Pekin', 'China', 'Art School 1', NULL, 38);
+
+INSERT into ADMIN.USERS (ROLE, EMAIL, PASSWORD, ISBLOCKED) VALUES (2, 'mindystuart@gmail.com', 'mindystuart', FALSE);
+INSERT INTO ADMIN.ENROLLEE (FULL_NAME, CITY, REGION, SCHOOL_NAME, CERTIFICATE, USER_ID) VALUES ('Mindy Stuart', 'Helena', 'Montana', 'School 39', NULL, 39);
+
+INSERT into ADMIN.USERS (ROLE, EMAIL, PASSWORD, ISBLOCKED) VALUES (2, 'markcuckerberg@gmail.com', 'markcuckerberg', FALSE);
+INSERT INTO ADMIN.ENROLLEE (FULL_NAME, CITY, REGION, SCHOOL_NAME, CERTIFICATE, USER_ID) VALUES ('Marc Cuckerberg', 'Madison', 'Wisconsin', 'Harvard School', NULL, 40);
+
+INSERT into ADMIN.USERS (ROLE, EMAIL, PASSWORD, ISBLOCKED) VALUES (2, 'katyperry@gmail.com', 'katyperry', FALSE);
+INSERT INTO ADMIN.ENROLLEE (FULL_NAME, CITY, REGION, SCHOOL_NAME, CERTIFICATE, USER_ID) VALUES ('Katy Perry', 'Frankfort', 'Kentucky', 'Art School 17', NULL, 41);
